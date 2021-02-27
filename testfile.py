906bd7787efbf2787ec67881cc5e5ba4453f38bb
philippbecker <der8ecker1@gmail.com>
2021-02-27 23:56:43 +0100

    test

---
+++ testfile.py
@@ -0,0 +1,22 @@
+print("wenn jonas das lesen kann schreib Herr Strauch ist cool in die while Schleife...")
+
+while("Herr Strauch ist cool"=="Herr Strauch ist cool"):
+    print("Herr Strauch ist cool")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
